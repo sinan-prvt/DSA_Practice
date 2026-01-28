1e720b8976875b67ee1b3347360ac320157de212
sinan-prvt <mohamedsinan9400@gmail.com>
2026-01-28 16:18:45 +0530

    Implemented-Dynamic-Array-with-Class

--- Array/DynamicArrayClass.py
+++ Array/DynamicArrayClass.py
@@ -42,47 +42,48 @@
 
 
 
-class DynamicArray:
-    def __init__(self):
-        self.capacity = 2
-        self.size = 0
-        self.arr = [None] * self.capacity
+# class DynamicArray:
+#     def __init__(self):
+#         self.capacity = 2
+#         self.size = 0
+#         self.arr = [None] * self.capacity
 
-    def display(self):
-        print("Array =", self.arr)
-        print("Size =", self.size)
-        print("Capacity =", self.capacity)
-        print("-" * 30)
+#     def display(self):
+#         print("Array =", self.arr)
+#         print("Size =", self.size)
+#         print("Capacity =", self.capacity)
 
-    def _resize(self, new_capacity):
-        new_arr = [None] * new_capacity
+#     def _resize(self, new_capacity):
+#         new_arr = [None] * new_capacity
 
-        for i in range(self.size):
-            new_arr[i] = self.arr[i]
+#         for i in range(self.size):
+#             new_arr[i] = self.arr[i]
 
-        self.arr = new_arr
-        self.capacity = new_capacity
+#         self.arr = new_arr
+#         self.capacity = new_capacity
 
-    def append(self, value):
-        if self.size == self.capacity:
-            self._resize(self.capacity * 2)
+#     def append(self, value):
+#         if self.size == self.capacity:
+#             self._resize(self.capacity * 2)
 
-        self.arr[self.size] = value
-        self.size += 1
+#         self.arr[self.size] = value
+#         self.size += 1
 
-    def get(self, index):
-        if index < 0 or index >= self.size:
-            print("Index out of bounds")
-            return None
-        return self.arr[index]
+#     def get(self, index):
+#         if index < 0 or index >= self.size:
+#             print("Index out of bounds")
+#             return None
+#         return self.arr[index]
 
-arr = DynamicArray()
+# arr = DynamicArray()
 
-arr.append(10)
-arr.append(20)
-arr.display()
+# arr.append(10)
+# arr.append(20)
+# arr.display()
 
-print("Element at index 1:", arr.get(1))
+# print("Element at index 1:", arr.get(1))
+
+
 
 
 # ======================== Insert an Array =============================== 
@@ -145,50 +146,50 @@ print("Element at index 1:", arr.get(1))
 # ======================== Delete an Array =============================== 
 
 
-# class DynamicArray:
-#     def __init__(self):
-#         self.capacity = 2
-#         self.size = 0
-#         self.arr = [None] * self.capacity
+class DynamicArray:
+    def __init__(self):
+        self.capacity = 2
+        self.size = 0
+        self.arr = [None] * self.capacity
     
-#     def display(self):
-#         print("Array =", self.arr)
-#         print("Capacity =", self.capacity)
-#         print("Size =", self.size)
+    def display(self):
+        print("Array =", self.arr)
+        print("Capacity =", self.capacity)
+        print("Size =", self.size)
     
-#     def resize(self, new_capacity):
-#         new_arr = [None] * new_capacity
+    def resize(self, new_capacity):
+        new_arr = [None] * new_capacity
 
-#         for i in range(self.size):
-#             new_arr[i] = self.arr[i]
+        for i in range(self.size):
+            new_arr[i] = self.arr[i]
         
-#         self.arr = new_arr
-#         self.capacity = new_capacity
+        self.arr = new_arr
+        self.capacity = new_capacity
     
-#     def append(self, value):
-#         if self.size == self.capacity:
-#             self.resize(self.capacity * 2)
+    def append(self, value):
+        if self.size == self.capacity:
+            self.resize(self.capacity * 2)
         
-#         self.arr[self.size] = value
-#         self.size += 1
+        self.arr[self.size] = value
+        self.size += 1
     
-#     def delete(self, index):
-#         if index < 0 or index > self.size:
-#             print("Invalid index")
-#             return
+    def delete(self, index):
+        if index < 0 or index > self.size:
+            print("Invalid index")
+            return
         
-#         for i in range(index, self.size - 1):
-#             self.arr[i] = self.arr[i + 1]
+        for i in range(index, self.size - 1):
+            self.arr[i] = self.arr[i + 1]
         
-#         self.arr[self.size - 1] = None
-#         self.size -= 1
+        self.arr[self.size - 1] = None
+        self.size -= 1
 
 
-# arr = DynamicArray()
+arr = DynamicArray()
 
-# arr.append(10)
-# arr.append(3)
-# arr.append(8)
+arr.append(10)
+arr.append(3)
+arr.append(8)
 
-# arr.delete(1)
-# arr.display()
+arr.delete(1)
+arr.display()
