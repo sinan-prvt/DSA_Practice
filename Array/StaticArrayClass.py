9aa85c8cb39ccf8208a682a87bcf53b1ab7e9656
sinan-prvt <mohamedsinan9400@gmail.com>
2026-01-28 12:40:31 +0530

    Implemented-Static-Array-with-Class

--- Array/StaticArrayClass.py
+++ Array/StaticArrayClass.py
@@ -52,35 +52,40 @@
 # ===================== Get Element =============================== o(1)
 
 
-class StaticArray:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.size = 0
-        self.arr = [None] * capacity
+# class StaticArray:
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.size = 0
+#         self.arr = [None] * capacity
 
-    def append(self, value):
-        if self.size == self.capacity:
-            print("Array is Full")
-            return
+#     def display(self):
+#         print("Array =", self.arr)
+#         print("Capacity =", self.capacity)
+#         print("Size =", self.size)
 
-        self.arr[self.size] = value
-        self.size += 1
+#     def append(self, value):
+#         if self.size == self.capacity:
+#             print("Array is Full")
+#             return
 
-    def get(self, index):
-        if index < 0 or index > self.size:
-            print("Invalid Index")
-            return None
-        return self.arr[index]
+#         self.arr[self.size] = value
+#         self.size += 1
+
+#     def get(self, index):
+#         if index < 0 or index > self.size:
+#             print("Invalid Index")
+#             return None
+#         return self.arr[index]
 
 
-arr = StaticArray(5)
+# arr = StaticArray(5)
 
-arr.append(10)
-arr.append(5)
-arr.append(9)
-arr.append(45)
+# arr.append(10)
+# arr.append(5)
+# arr.append(9)
+# arr.append(45)
 
-print("Element at index 1 =", arr.get(1))
+# print("Element at index 1 =", arr.get(1))
 
 
 
@@ -135,41 +140,41 @@ print("Element at index 1 =", arr.get(1))
 # ===================== Delete Element =============================== o(n)
 
 
-# class StaticArray:
-#     def __init__(self, capacity):
-#         self.capacity = capacity
-#         self.size = 0
-#         self.arr = [None] * capacity
+class StaticArray:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.arr = [None] * capacity
 
-#     def display(self):
-#         print("Array =", self.arr)
-#         print("Capacity =", self.capacity)
-#         print("Size =", self.size)
+    def display(self):
+        print("Array =", self.arr)
+        print("Capacity =", self.capacity)
+        print("Size =", self.size)
 
-#     def append(self, value):
-#         if self.size == self.capacity:
-#             print("Array is Full")
-#             return
+    def append(self, value):
+        if self.size == self.capacity:
+            print("Array is Full")
+            return
         
-#         self.arr[self.size] = value
-#         self.size += 1
+        self.arr[self.size] = value
+        self.size += 1
     
-#     def delete(self, index):
-#         if index < 0 or index > self.size:
-#             print("Invalid Index")
-#             return
+    def delete(self, index):
+        if index < 0 or index > self.size:
+            print("Invalid Index")
+            return
     
-#         for i in range(index, self.size - 1):
-#             self.arr[i] = self.arr[i + 1]
+        for i in range(index, self.size - 1):
+            self.arr[i] = self.arr[i + 1]
 
-#         self.arr[self.size - 1] = None
-#         self.size += 1
+        self.arr[self.size - 1] = None
+        self.size += 1
 
-# arr = StaticArray(5)
+arr = StaticArray(5)
 
-# arr.append(10)
-# arr.append(3)
-# arr.append(7)
+arr.append(10)
+arr.append(3)
+arr.append(7)
 
-# arr.delete(1)
-# arr.display()
+arr.delete(1)
+arr.display()
